d4f0014a24e5938d55c6caea095e5763b0af70ee
paalex <salexp@gmail.com>
2020-04-10 23:07:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ import _ from 'lodash'
 import {config as dotenv_config} from 'dotenv'
 
 dotenv_config()
-console.log(`Your port is ${process.env.PORT}`); // 3000
+// console.log(`Your port is ${process.env.PORT}`); // 3000
 
 const {promise: gpiop} = gpio;
-// Relay GPIOs - 6 13 19 26
-gpio.setup(6, gpio.DIR_OUT);
+// Relay GPIOs - 31 33 35 37 (physical pin #)
+gpio.setup(31, gpio.DIR_OUT);
 
 async function main() {
   _.forEach(new Array(5), (some, idx) => {
     setTimeout(() => {
-      gpio.write(6, false)
+      gpio.write(31, false)
       console.log('Pin 6 off')
     }, idx * 1000 );
     setTimeout(() => {
