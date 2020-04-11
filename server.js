a65875fa5f76066846d9fa47df91618f38bedabb
paalex <salexp@gmail.com>
2020-04-10 22:46:30 -0400

    testing script

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ dotenv_config()
 console.log(`Your port is ${process.env.PORT}`); // 3000
 
 const {promise: gpiop} = gpio;
+// Relay GPIOs - 6 13 19 26
+gpio.setup(6, gpio.DIR_OUT);
 
-// gpio.setup(7, gpio.DIR_IN, readInput);
-// function readInput(err) {
-//   if (err) throw err;
-//   gpio.read(7, function (err, value) {
-//     if (err) throw err;
-//     console.log('The value is ' + value);
-//   });
-// }
+async function main() {
+  _.forEach(new Array(5), (some, idx) => {
+    setTimeout(() => gpio.write(6, false), idx * 2000 );
+    setTimeout(() => gpio.write(6, true), (idx + 1) * 2000);
+  })
+}
+main().catch(e => console.warn(e))
 //
 // gpiop.setup(7, gpiop.DIR_OUT)
 //   .then(() => {
