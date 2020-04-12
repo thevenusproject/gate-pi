e7ff0effb8c04c5f2402f5360589099c14d877b3
paalex <salexp@gmail.com>
2020-04-12 09:34:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const {promise: gpiop} = gpio;
 // Relay GPIOs - 31 33 35 37 (physical pin #)
 // gpiop.setup(31, gpio.DIR_HIGH).then(() => main()).catch(e => console.warn(e));
 
-async function main() {
+async function setupPhysicalPins() {
   await gpiop.setup(31, gpio.DIR_HIGH).catch(e => console.warn(e)); // Open
   await gpiop.setup(33, gpio.DIR_HIGH).catch(e => console.warn(e)); // Open
   await gpiop.setup(35, gpio.DIR_HIGH).catch(e => console.warn(e)); // Open
@@ -27,27 +27,35 @@ async function main() {
   // cycleRelay37Demo().catch(e => console.warn(e));
 }
 
+async function setupBlynkPins() {
 
-v1.on('write', async function(params) {
-  // Cycle gate
-  writePinFromBlynk({pin: 31, params})
-});
+  v1.on('write', async function (params) {
+    // Cycle gate
+    writePinFromBlynk({pin: 31, params})
+  });
 
-v2.on('write', async function(params) {
-  // Disable button
-  writePinFromBlynk({pin: 33, params})
-});
+  v2.on('write', async function (params) {
+    // Disable button
+    writePinFromBlynk({pin: 33, params})
+  });
 
-v3.on('write', async function(params) {
-  // Open gate
-  writePinFromBlynk({pin: 35, params})
-});
+  v3.on('write', async function (params) {
+    // Open gate
+    writePinFromBlynk({pin: 35, params})
+  });
+}
 
 function writePinFromBlynk({pin, params}) {
   const value = _.get(params,'[0]') !== "1";
   gpiop.write(pin, value).catch(e => console.log(`error setting pin ${pin}`, e))
 }
 
+async function setup() {
+  await setupPhysicalPins()
+  await setupBlynkPins()
+}
+
+setup().catch(e => console.log('err in pins setup', e));
 //
 // v9.on('read', function() {
 //   v9.write(new Date().getSeconds());
