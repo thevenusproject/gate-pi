aecfc4195259501813edbd66de5383ef8c8689cb
paalex <salexp@gmail.com>
2020-12-15 00:42:47 +0300

    debug

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ async function externalSensorPolling() {
             }
           }
         }
-        // console.log('coolDownNotificationsCounter', coolDownNotificationsCounter)
+        console.log('coolDownNotificationsCounter', coolDownNotificationsCounter, 'triggerCounter',triggerCounter)
       } else {
         triggerCounter += 1;
       }
