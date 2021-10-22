b00bcf875967dee808219d5567c032a77c8ef4d3
lllll <xxxx@xxxx.xxx>
2021-10-22 03:26:20 +0300

    logging

--- store.js
+++ store.js
@@ -15,6 +15,7 @@ export function initStore() {
 
 export function getSetting({ setting }) {
   const sett = nconf.get(`settings:${setting}`);
+  console.error('sett', sett, typeof sett)
   if (sett !== true && sett !== false) console.error('invalid sett', sett, typeof sett)
   return sett;
 }
